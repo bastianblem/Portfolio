419e39bf801f8dc8f2548a952e8c1c89032a9bba
bastianblem <bastian.buettner@student.uni-luebeck.de>
2026-07-01 22:28:23 +0200

    Updated the index page. Implemeted Hero, ProjectDisplay and Contact(Still empty).
    Created a Card Component.(No Content yet)

---
+++ app/components/cards/ProjectCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- Title, Subtitles and Tags -->
+    <div>
+      <!-- Title -->
+      <!-- Subtitle -->
+      <!-- Tags -->
+    </div>
+    <!-- Project Description -->
+    <div></div>
+    <!-- CTA Section -->
+    <div></div>
+  </div>
+</template>
+<script setup lang="ts"></script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,2 +1,10 @@
-<template></template>
-<script setup lang="ts"></script>
+<template>
+  <HeroSection />
+  <ProjectDisplay />
+  <ContactSection />
+</template>
+<script setup lang="ts">
+import ProjectDisplay from "~/components/ProjectDisplay.vue";
+import ContactSection from "~/components/sections/ContactSection.vue";
+import HeroSection from "~/components/sections/HeroSection.vue";
+</script>
